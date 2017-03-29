a6082643f9f357a39c5367b1689a5e15549fca13
®•ابــو ســكروــب)•® <almansorr98@gmail.com>
2017-03-29 15:44:24 +0300

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,32 @@
+do local _ = {
+  admins = {
+    {
+      0
+    },
+    {
+      189161839,
+      "@iq\\_313k"
+    }
+  },
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "groupmanager",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "reply",
+    "echo",
+    "info"
+  },
+  info_text = "",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    115124695,
+    0
+  }
+}
+return _
+end

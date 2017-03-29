6f274268983edadfda5b484f6c9d4fbc010a9c28
®•ابــو ســكروــب)•® <almansorr98@gmail.com>
2017-03-29 15:59:12 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+
+
+cd EVO_TEAM 
+
+chmod +x evo.sh
+
+./evo.sh install
+
+./evo.sh
+
+
+
+--install.sh with lua by EVO_TEAM
+--ch: @dev_evo
+--botdev: @EVOtmBot
+--devlober: @abod_evo
